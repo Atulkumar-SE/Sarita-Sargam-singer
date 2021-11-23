f934d9bb96ff44a9ef826f1ddcd056144ae70814
Atul Jaiswal <atuljaiswal.se@gmail.com>
2021-11-23 22:55:45 +0530

    Navigation bar is completed

--- script/script_1.js
+++ script/script_1.js
@@ -1,3 +1,11 @@
-prompt("write your name:");
-alert("Welcome");
-document.write("Your most Welcome in this web")
+const navMenu = document.getElementById('nav_menu'),
+toggleMenu = document.getElementById('toggle_menu'),
+closeMenu = document.getElementById('close_menu')
+
+toggleMenu.addEventListener('click', () => {
+    navMenu.classList.toggle('show')
+})
+
+closeMenu.addEventListener('click', () => {
+    navMenu.classList.remove('show')
+})
