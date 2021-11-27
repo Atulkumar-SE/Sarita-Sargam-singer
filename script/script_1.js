f56b810c5643a3eb96d6baf8791c380e7fd38c3e
Atul Jaiswal <atuljaiswal.se@gmail.com>
2021-11-27 20:34:34 +0530

    adding top button and fixing close menu btn as well as latest video div

--- script/script_1.js
+++ script/script_1.js
@@ -9,3 +9,25 @@ toggleMenu.addEventListener('click', () => {
 closeMenu.addEventListener('click', () => {
     navMenu.classList.remove('show')
 })
+
+//-------------------------top button--------------------
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 722 || document.documentElement.scrollTop > 722) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
