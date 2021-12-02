1eee3ac7317040f20966c2aa2432a7f571fe5033
Atul Jaiswal <atuljaiswal.se@gmail.com>
2021-12-02 19:38:54 +0530

    video section is done

--- script/script_1.js
+++ script/script_1.js
@@ -31,3 +31,33 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+//---------------------Video slider-----------------------
+
+
+let flag = 0;
+
+function contoller(x){
+    flag = flag + x;
+    slideshow(flag);
+}
+
+slideshow(flag);
+
+function slideshow(num){
+  let slides = document.getElementsByClassName('slide');
+
+    if(num == slides.length){
+        flag = 0;
+        num = 0;
+    }
+    if(num < 0){
+        flag = slides.length-1;
+        num = slides.length-1;
+    }
+  for(let y of slides){
+      y.style.display = "none";
+  }
+
+    slides[num].style.display = "block";
+}
